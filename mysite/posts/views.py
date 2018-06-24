6b92ef74c5cc9f79ce126ec50ededd001fcd472f
Takashi Ikki <takahashi@omicale.com>
2018-06-25 00:51:11 +0900

    add template file

--- mysite/posts/views.py
+++ mysite/posts/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello World!! このページは投稿のインデックスです')
+    # return HttpResponse('Hello World!! このページは投稿のインデックスです')
+    return render(request, 'posts/index.html')
